132293186a3dc5131ee0631520b70188533496c7
Chris Sharp <christopher.sharp@hotmail.co.uk>
2026-08-28 18:05:50 +0100

    Increase version numbers

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -28,7 +28,7 @@ export default ({ config }: ConfigContext) => {
     android: {
       package: bundleId,
       icon: "./src/assets/logos/eventful-logo-android.png",
-      versionCode: 312,
+      versionCode: 313,
       softwareKeyboardLayoutMode: "pan",
       permissions: [
         "android.permission.READ_EXTERNAL_STORAGE",
@@ -43,7 +43,7 @@ export default ({ config }: ConfigContext) => {
     ios: {
       bundleIdentifier: bundleId,
       icon: "./src/assets/logos/eventful-logo.png",
-      buildNumber: "317",
+      buildNumber: "318",
       supportsTablet: true,
       infoPlist: {
         NSPhotoLibraryUsageDescription:
