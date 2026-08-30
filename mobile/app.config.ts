761b8a7ef9fb937ce454297082e4aeef03da76fc
Chris Sharp <christopher.sharp@hotmail.co.uk>
2026-08-30 16:09:42 +0100

    Increase version to 6.4.0

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -15,8 +15,8 @@ export default ({ config }: ConfigContext) => {
     slug: "Eventful",
     scheme: "eventful",
     owner: "chrissharp",
-    version: "6.3.0",
-    runtimeVersion: "6.3.0",
+    version: "6.4.0",
+    runtimeVersion: "6.4.0",
     orientation: "portrait",
     icon: "./src/assets/logos/eventful-logo.png",
     userInterfaceStyle: "light",
@@ -28,7 +28,7 @@ export default ({ config }: ConfigContext) => {
     android: {
       package: bundleId,
       icon: "./src/assets/logos/eventful-logo-android.png",
-      versionCode: 325,
+      versionCode: 327,
       softwareKeyboardLayoutMode: "pan",
       permissions: [
         "android.permission.READ_EXTERNAL_STORAGE",
@@ -43,7 +43,7 @@ export default ({ config }: ConfigContext) => {
     ios: {
       bundleIdentifier: bundleId,
       icon: "./src/assets/logos/eventful-logo.png",
-      buildNumber: "330",
+      buildNumber: "332",
       supportsTablet: true,
       infoPlist: {
         NSPhotoLibraryUsageDescription:
