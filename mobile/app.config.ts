0c4e2eb10735be5752de6ea30c0f6a03d765717d
Chris Sharp <christopher.sharp@hotmail.co.uk>
2026-08-31 18:32:09 +0100

    Update build numbers

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -28,7 +28,7 @@ export default ({ config }: ConfigContext) => {
     android: {
       package: bundleId,
       icon: "./src/assets/logos/eventful-logo-android.png",
-      versionCode: 329,
+      versionCode: 330,
       softwareKeyboardLayoutMode: "pan",
       permissions: [
         "android.permission.READ_EXTERNAL_STORAGE",
@@ -43,7 +43,7 @@ export default ({ config }: ConfigContext) => {
     ios: {
       bundleIdentifier: bundleId,
       icon: "./src/assets/logos/eventful-logo.png",
-      buildNumber: "334",
+      buildNumber: "335",
       supportsTablet: true,
       infoPlist: {
         NSPhotoLibraryUsageDescription:
