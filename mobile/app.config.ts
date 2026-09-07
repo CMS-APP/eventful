adbfd6f5eb7a4e45ce4d22ad6b7dd4edf8991d20
Chris Sharp <christopher.sharp@hotmail.co.uk>
2026-09-07 12:07:56 +0100

    Update app version and build numbers in configuration

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -15,8 +15,8 @@ export default ({ config }: ConfigContext) => {
     slug: "Eventful",
     scheme: "eventful",
     owner: "chrissharp",
-    version: "6.11.1",
-    runtimeVersion: "6.11.1",
+    version: "6.12.0",
+    runtimeVersion: "6.12.0",
     orientation: "portrait",
     icon: "./src/assets/logos/eventful-logo.png",
     userInterfaceStyle: "light",
@@ -28,7 +28,7 @@ export default ({ config }: ConfigContext) => {
     android: {
       package: bundleId,
       icon: "./src/assets/logos/eventful-logo-android.png",
-      versionCode: 356,
+      versionCode: 357,
       softwareKeyboardLayoutMode: "pan",
       permissions: [
         "android.permission.READ_EXTERNAL_STORAGE",
@@ -44,7 +44,7 @@ export default ({ config }: ConfigContext) => {
     ios: {
       bundleIdentifier: bundleId,
       icon: "./src/assets/logos/eventful-logo.png",
-      buildNumber: "363",
+      buildNumber: "364",
       supportsTablet: true,
       appleTeamId: "4LMZHG2P3T",
       infoPlist: {
