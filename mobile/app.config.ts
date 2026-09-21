669dabda56394e255ce2f324ffff7bf5eecf0d4c
Chris Sharp <christopher.sharp@hotmail.co.uk>
2026-09-21 23:03:27 +0100

    Updated versions

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -28,7 +28,7 @@ export default ({ config }: ConfigContext) => {
     android: {
       package: bundleId,
       icon: "./src/assets/logos/eventful-logo-android.png",
-      versionCode: 371,
+      versionCode: 372,
       softwareKeyboardLayoutMode: "pan",
       permissions: [
         "android.permission.READ_EXTERNAL_STORAGE",
@@ -44,7 +44,7 @@ export default ({ config }: ConfigContext) => {
     ios: {
       bundleIdentifier: bundleId,
       icon: "./src/assets/logos/eventful-logo.png",
-      buildNumber: "377",
+      buildNumber: "378",
       supportsTablet: true,
       appleTeamId: "4LMZHG2P3T",
       infoPlist: {
